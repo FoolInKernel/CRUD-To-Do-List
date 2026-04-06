635536ff99a0921db224a2ce5f88ad7e3b41bd0f
FoolInKernel <fuwannnn@gmail.com>
2026-04-06 07:14:49 +0700

    p

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include "service/database.php" ;
 
+// Create
 if(isset($_POST['ok'])) {
 $hari = $_POST['hari'];
 $akti = $_POST['akti'];
@@ -12,6 +13,14 @@ $akti = $_POST['akti'];
 
 }
 
+//Delete
+if(isset($_POST['hapus'])) {
+    $id = $_POST['id'] ;
+    mysqli_query($conn, "DELETE FROM list WHERE id = $id");
+}
+
+//Update
+
 
 ?>
 
@@ -20,6 +29,7 @@ $akti = $_POST['akti'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>CRUD</title>
 </head>
 <body>
@@ -66,13 +76,17 @@ $akti = $_POST['akti'];
             <td>{$row['akti']}</td>
             <td>{$row['created_at']}</td>
             <td>
-                <a href='edit.php?id={$row['id']}'>Edit</a> |
-                <a href='delete.php?id={$row['id']}'>Hapus</a>
-            </td>
+                    <a href='index.php?edit={$row['id']}'>Edit</a> |
+                    <form action='index.php' method='post' style='display:inline'>
+                        <input type='hidden' name='id' value='{$row['id']}'>
+                        <button name='hapus' type='submit'>Hapus</button>
+                    </form>
+                </td>
         </tr>";
         $no++;
     }
     ?>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
