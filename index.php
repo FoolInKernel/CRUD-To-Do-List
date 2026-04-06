1479bb02a675b6776044cd6300505734085af8c0
Nicky <fuwannnn@gmail.com>
2026-04-06 00:46:19 +0000

    modal edit

--- index.php
+++ index.php
@@ -87,6 +87,31 @@ if(isset($_POST['hapus'])) {
     }
     ?>
 
+<td>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="modaledit">
+  Buka Modal
+</button>
+</td>
+
+
+<div class="modal" tabindex="-1"  id="modaledit">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
